1051e656f02439794d44c00a1fb7513b9f3f3e9a
Julien Tessier <julien@cahri.com>
2015-06-09 09:22:11 +0400

    bugfix sur les critères avec la date

--- sondage_criteres.php
+++ sondage_criteres.php
@@ -5,20 +5,18 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 include_spip('sondage_fonctions');
 
 function critere_sondages_en_cours($idb, &$boucles, $crit) {
-	$maintenant = date('Y-m-d H:i:s');
 	$boucle = &$boucles[$idb];
-	$boucle->where[] = array("'<='", "'{$boucle->id_table}.date_debut'", "'\"".$maintenant."\"'");
-	$boucle->where[] = array("'>='", "'{$boucle->id_table}.date_fin'", "'\"".$maintenant."\"'");
+	$boucle->where[] = array("'<='", "'{$boucle->id_table}.date_debut'", "'NOW()'");
+	$boucle->where[] = array("'>='", "'{$boucle->id_table}.date_fin'", "'NOW()'");
 }
 
 function critere_sondages_termines($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
-	$boucle->where[] = array("'<'", "'{$boucle->id_table}.date_fin'", "'\"".date('Y-m-d H:i:s')."\"'");
+	$boucle->where[] = array("'<'", "'{$boucle->id_table}.date_fin'", "'NOW()'");
 }
 
 function critere_sondages_a_venir($idb, &$boucles, $crit) {
 	$boucle = &$boucles[$idb];
-	$boucle->where[] = array("'>'", "'{$boucle->id_table}.date_debut'", "'\"".date('Y-m-d H:i:s')."\"'");
+	$boucle->where[] = array("'>'", "'{$boucle->id_table}.date_debut'", "'NOW()'");
 }
-
 
